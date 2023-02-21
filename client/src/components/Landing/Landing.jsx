f818ae4a65e27165af8bc9170d3b10ef0ad39218
Adnan Ali <adnanali6688@gmail.com>
2023-02-22 00:54:52 +0100

    implemented header

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,18 +1,37 @@
 import styles from "./Landing.module.scss";
+import { useState } from "react";
 
 const Landing = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const hamburgerHanlder = () => {
+    setIsActive(!isActive);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.navigation}>
         <div className={styles.logo}>Logo</div>
         <div className={styles.linksWrapper}>
           <ul className={styles.navLinks}>
-            <li>About</li>
-            <li>Project</li>
-            <li>Contant</li>
+            <li className={styles.link}>About</li>
+            <li className={styles.link}>Project</li>
+            <li className={styles.link}>Contant</li>
           </ul>
         </div>
-        <div className={styles.burger}>Hamburger</div>
+        <div onClick={hamburgerHanlder} className={styles.burger}>
+          {!isActive ? (
+            <img
+              className={styles.menuIcon}
+              src={process.env.PUBLIC_URL + "media/images/menu.png"}
+            />
+          ) : (
+            <img
+              className={styles.menuIcon}
+              src={process.env.PUBLIC_URL + "media/images/cross.png"}
+            />
+          )}
+        </div>
       </div>
       <div className={styles.body}>Body</div>
       <div className={styles.footer}>Footer</div>
