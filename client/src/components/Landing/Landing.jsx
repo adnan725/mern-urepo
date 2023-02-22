9cda851a899e07e131235beb9a89adc1cfe6a46e
Adnan Ali <adnanali6688@gmail.com>
2023-02-22 02:25:42 +0100

    implemented hamburger menubar logic

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,9 +1,22 @@
 import styles from "./Landing.module.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SideBar from "../SideBar/SideBar";
 
 const Landing = () => {
   const [isActive, setIsActive] = useState(false);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  });
 
   const hamburgerHanlder = () => {
     setIsActive(!isActive);
@@ -11,31 +24,26 @@ const Landing = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.sidebar}>
-        <SideBar />
-      </div>
+      <div className={styles.sidebar}>{isActive && <SideBar />}</div>
       <div className={styles.navigation}>
         <div className={styles.logo}>Logo</div>
-        <div className={styles.linksWrapper}>
-          <ul className={styles.navLinks}>
-            <li className={styles.link}>About</li>
-            <li className={styles.link}>Project</li>
-            <li className={styles.link}>Contant</li>
-          </ul>
-        </div>
-        {/*  <div onClick={hamburgerHanlder} className={styles.burger}>
-          {!isActive ? (
+
+        {windowWidth <= "768" ? (
+          <div onClick={hamburgerHanlder} className={styles.burger}>
             <img
               className={styles.menuIcon}
               src={process.env.PUBLIC_URL + "media/images/menu.png"}
             />
-          ) : (
-            <img
-              className={styles.menuIcon}
-              src={process.env.PUBLIC_URL + "media/images/cross.png"}
-            />
-          )}
-        </div> */}
+          </div>
+        ) : (
+          <div className={styles.linksWrapper}>
+            <ul className={styles.navLinks}>
+              <li className={styles.link}>About</li>
+              <li className={styles.link}>Project</li>
+              <li className={styles.link}>Contant</li>
+            </ul>
+          </div>
+        )}
       </div>
       <div className={styles.body}>
         <div className={styles.bodyWrapper}>
